5e9b075d34eb16ff3c7729e7c78742a7b9f3375a
Anderson <yauripachas@gmail.com>
2026-09-20 22:29:10 -0500

    feat(app): agregar modelo de datos TrafficIndicator

---
+++ app/src/main/java/com/anderson/wifiprevent/domain/model/TrafficIndicator.kt
@@ -0,0 +1,8 @@
+package com.anderson.wifiprevent.domain.model
+
+data class TrafficIndicator(
+    val code: String,
+    val severity: String,
+    val title: String,
+    val description: String
+)
